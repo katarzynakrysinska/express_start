a656360208916544e3bf9271bbbb9895cbe3e4e1
Katarzyna Krysińska <katarzyna.krysinska@onet.pl>
2022-02-16 16:43:11 +0100

    endpoints with middleware

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.listen(9000, () => {
-  console.log('Server is running on port: 9000');
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/contact.html'));
+});
+
+app.get('/info', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/info.html'));
+});
+
+app.get('/history', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/history.html'));
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/style.css'));
+});
+
+app.get('/test.png', (req, res) => {
+  res.sendFile(path.join(__dirname, '/test.png'));
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
 });
